c19484c4809d4e5b053591136354920791f95c8e
stojics1 <stojics@mymacewan.ca>
2024-01-22 19:50:57 -0700

    Logic For isArrayFactorChain.js completed

---
+++ isArrayFactorChain.js
@@ -0,0 +1,16 @@
+// const _ = require("CMPT315_A2_SS");
+
+function isArrayFactorChain(list){
+    let len = list.length;
+    let bool = false;        
+    for(let i = 0; i < len - 1 ; i++){ //interats through the list
+    
+        if(list[i + 1] % list[i] == 0){ // checks if the next element is a factor, sets bool to true if yes else false
+            bool = true;
+        }
+        else{
+            bool = false;
+        };
+    };
+    return bool;
+};
